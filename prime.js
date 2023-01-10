17ba8114d56ea204df9105be5e3095ab33a53469
unknown <sawantaviraj2@gmail.com>
2023-01-10 23:48:01 +0530

    Code corrected

--- prime.js
+++ prime.js
@@ -1,11 +1,9 @@
 let a=5;
 let count=0;
-for(i=0; i<=a; i++){
-if(a%i==0){
+for(let i=1; i<=a; i++){
+if(a%i==0)
+{
 count++;
 }
-if(count==2);
-console.log("Prime");
-else{
-console.log("Not prime);
+(count==2) ? console.log("Prime") : console.log("Not a Prime");
 
